308b2be7e4bc3446cc22d80dc71a5a2ababd5d24
Wei Peng <write.to.peng.wei@gmail.com>
2014-05-23 23:56:40 -0400

    build-doc-javadoc: clear up legacy tmpdir

--- 02build-doc-javadoc.sh
+++ 02build-doc-javadoc.sh
@@ -6,8 +6,9 @@ TMPDIR=${TMPDIR:-/tmp/aosp-javadoc/}
 api_dirs=( frameworks/base/core/java/ frameworks/base/services/java/ libcore/libdvm/src/main/java/ libcore/dalvik/src/main/java/ libcore/luni/src/main/java )
 api_subpackages=android:com:dalvik:java:javax:org:sun
 
+# clear legacy
+rm -rf ${TMPDIR}
 mkdir -p ${TMPDIR}
-echo ${TMPDIR} created
 
 for dir in "${api_dirs[@]}"; do
     cp -r ${dir}/* ${TMPDIR}
